444922e85b1162a756cea9be515543f592a6eb8f
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-09-25 17:20:25 +0200

    move build in docs folder

--- Assets/RotateCoin.cs
+++ Assets/RotateCoin.cs
@@ -13,7 +13,7 @@ public class RotateCoin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(1, 0, 0), 0.5f);
+        transform.Rotate(new Vector3(1, 0, 0), 0.2f);
         
     }
 
